ea41c8ee52116fcb6c495213919c156ce18e8eca
SavkoMax <savcomax@gmail.com>
2022-08-26 18:05:05 +0300

    FIX

--- build_tools/build_source.sh
+++ build_tools/build_source.sh
@@ -8,5 +8,10 @@ cd ../../
 python -m venv build_env
 source build_env/bin/activate
 
+python -m pip install numpy, scipy, matplotlib
+python -m pip install twine
+
 cd ML-handmade/ML-handmade
-python -m pip install .[dev]
+python setup.py sdist
+
+twine check dist/*.tar.gz
