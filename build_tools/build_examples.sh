dbe114aeafa93053dcdec26839285747ed4836be
SavkoMax <savcomax@gmail.com>
2022-08-26 17:43:30 +0300

    FIX

--- build_tools/build_examples.sh
+++ build_tools/build_examples.sh
@@ -11,10 +11,10 @@ source examples_env/bin/activate
 python -m pip install jupyter pandas scikit-learn
 
 cd ML-handmade/ML-handmade/examples
-for note in *.ipynb do
-    jupyter nbconvert --to html --execute $note
-    if [ $? != 0 ] then
-        echo $note
+for note in *.ipynb
+    do jupyter nbconvert --to html --execute $note
+    if [ $? != 0 ]
+        then echo $note
         exit 1
     fi
 done
